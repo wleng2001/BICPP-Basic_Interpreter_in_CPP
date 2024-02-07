f13499abd7389493c858f45fd7400d01e72110b7
wleng2001 <kamilgrodzki5@gmail.com>
2024-02-07 15:43:15 +0100

    add partial arduino support in text_analyzer.h

--- text_analyzer.h
+++ text_analyzer.h
@@ -1,10 +1,15 @@
 #ifndef text_analyzer_h
 #define text_analyzer_h
 
+#include "config.h"
+#include "tablesAndOtherConst.h"
+
+#if arduino
+
+#else
 #include <iostream>
 #include <stdio.h> //aby działał getchar i exit
 #include <iomanip> //biblioteka do manipulaowania wejściem i wyjściem
-#include <cmath>
 #ifdef _WIN32
 #include <windows.h>
 #include <conio.h>
@@ -14,11 +19,10 @@
 #include <cstdlib> //biblioteka, aby działały komendy sytemowe np: system("cls");
 #include <time.h>
 #include <fstream> //biblioteka do obsługi plików
+#endif
+
+#include <cmath>
 #include <algorithm>
-
-#include "tablesAndOtherConst.h"
-
-//#define debug true
 
 using namespace std;
 
@@ -26,18 +30,34 @@ class text_analyzer{
   private:
 
   int _position = 0;
+  #if arduino
+  void (*_errorFunc)(String text);
+  #else
   void (*_errorFunc)(string text);
+  #endif
 
+  #if arduino
+  bool special_char_in_correct_place(String &data, int position);
+  #else
   bool special_char_in_correct_place(string &data, int position);
+  #endif
   bool correctBracketQuantity(uint8_t *bracketQuantity, char c);
 
   public:
 
     bool *error;
 
+    #if arduino
+    void addErrorFuntion(void (*errorFunction)(String text));
+    #else
     void addErrorFuntion(void (*errorFunction)(string text));
+    #endif
     
+    #if arduino
+    String rewrite_in_range(uint8_t start, uint8_t end, String data) {
+    #else
     string rewrite_in_range(uint8_t start, uint8_t end, string data) {
+    #endif
       string new_data = "";
       if (end < 0) {
         return "";
@@ -48,7 +68,11 @@ class text_analyzer{
       return new_data;
     }
 
+    #if arduino
+    void delete_spaces(String &data) {
+    #else
     void delete_spaces(string &data) {
+    #endif
       uint8_t i = 0;
       char analize_char = data[0];
       string new_data = "";
@@ -62,15 +86,27 @@ class text_analyzer{
       data = new_data;
     }
 
+    #if arduino
+    String rm_comment(String data) {
+    #else
     string rm_comment(string data) {
+    #endif
       if(data.find("REM")==string::npos){
         return data;
       }
+      #if arduino
+      String new_data = rewrite_in_range(0, data.find("REM")-1, data);
+      #else
       string new_data = rewrite_in_range(0, data.find("REM")-1, data);
+      #endif
       return new_data;
     }
 
+    #if arduino
+    String trim(String data){
+    #else
     string trim(string data){
+    #endif
       int data_len = data.length();
       char sign=data[data_len-1];
       while(sign==' '){
@@ -80,8 +116,13 @@ class text_analyzer{
       return rewrite_in_range(0, data_len-1, data);
     }
 
+    #if arduino
+    void up_case(String &data){
+      String new_data="";
+    #else
     void up_case(string &data){
       string new_data="";
+    #endif
       bool start_quotation=0;
       int data_len = data.length();
       for(uint8_t i = 0; i<data_len; i++){
@@ -97,11 +138,19 @@ class text_analyzer{
       data = new_data;
     }
 
+    #if arduino
+    void delete_useless_spaces(String &data);
+
+    String one_word_ret(String data);
+
+    String reform_input(String &data);
+    #else
     void delete_useless_spaces(string &data);
 
     string one_word_ret(string data);
 
     string reform_input(string &data);
+    #endif
 };
 
 #endif
